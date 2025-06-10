4fae1cfcb8c76ef7c2c442895f86850a0e16141c
YANG-XIAOLI123 <yxl980816@gmail.com>
2025-06-10 11:11:20 +0200

    Update sketch-utils.js

--- sketch-utils.js
+++ sketch-utils.js
@@ -1,16 +1,43 @@
 let colore_emozioni = [];
 let linea_emozioni = [];
+let immagini_disegnate=[]
 let font;
 let parola = "EMOLAND";
-let size = random(20, 150); 
+
 
 function preload() {
   font = loadFont("./fonts/EMOLAND.ttf"); 
+  colore_emozioni=[
+    loadImage( "./assets/colore/1.svg"),
+    loadImage("./assets/colore/2.svg"),
+    loadImage("./assets/colore/3.svg"),
+    loadImage("./assets/colore/4.svg"),
+    loadImage("./assets/colore/5.svg"),
+    loadImage("./assets/colore/6.svg"),
+    loadImage("./assets/colore/7.svg"),
+    loadImage("./assets/colore/8.svg"),
+  ]
+ linea_emozioni = [
+   loadImage ("./assets/linea/01.svg"),
+   loadImage ("./assets/linea/02.svg"),
+   loadImage ("./assets/linea/03.svg"),
+   loadImage ("./assets/linea/04.svg"),
+   loadImage ("./assets/linea/05.svg"),
+   loadImage ("./assets/linea/06.svg"),
+   loadImage ("./assets/linea/07.svg"),
+   loadImage ("./assets/linea/08.svg"),
+  ];
 }
 
 function setup() {
+  imageMode(CENTER)
   createCanvas(windowWidth, windowHeight); 
-  background(255);
+
+
+ 
+}
+function draw(){
+   background(255);
 
 
   textFont(font);
@@ -19,42 +46,26 @@ function setup() {
   fill(0);
   text(parola, width / 2, height / 2);
 
-  
-  colore_emozioni = [
-    "./assets/colore/1.svg",
-    "./assets/colore/2.svg",
-    "./assets/colore/3.svg",
-    "./assets/colore/4.svg",
-    "./assets/colore/5.svg",
-    "./assets/colore/6.svg",
-    "./assets/colore/7.svg",
-    "./assets/colore/8.svg",
-  ];
-
-  linea_emozioni = [
-    "./assets/linea/01.svg",
-    "./assets/linea/02.svg",
-    "./assets/linea/03.svg",
-    "./assets/linea/04.svg",
-    "./assets/linea/05.svg",
-    "./assets/linea/06.svg",
-    "./assets/linea/07.svg",
-    "./assets/linea/08.svg",
-  ];
+for (let i of immagini_disegnate) {
+  push()
+  translate(i.x, i.y)
+  rotate(frameCount)
+ image(i.forma, 0, 0, i.dimensione, i.dimensione)
+  pop()
 }
+
+}
+
 
 function mousePressed() {
   let chosenPath = random(random() < 0.5 ? colore_emozioni : linea_emozioni);
 
-  let svgElemento = createImg(chosenPath, "svg-image");
-  svgElemento.position(mouseX, mouseY);
-
-  let size = random(50, 150); // 随机尺寸
-  svgElemento.size(size, size);
-
-  svgElemento.style('pointer-events', 'none');
-
- setTimeout(() => {
-svgElemento.remove();
- }, 10000);
+ immagini_disegnate.push(
+  {
+    forma:chosenPath,
+    x:mouseX,
+    y:mouseY,
+    dimensione:random(20,100)
+  }
+ )
 }
